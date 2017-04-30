f155fe7360bb78e6de556f3b90d5aa770c68c108
Jeff Handley <jeff@jeffhandley.com>
2017-04-29 17:28:34 -0700

    Recursively traverse the source directory

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { argv } from 'yargs';
 import fs from 'fs';
+import path from 'path';
 import { ExifImage } from 'exif';
 
 const src = argv.s || argv.src || argv.source;
@@ -11,15 +12,38 @@ if (!src || !fs.statSync(src).isDirectory()) {
     process.exit(1);
 }
 
-function processDirectory(directory) {
-    fs.readdir(src, (err, files) => {
-        files.forEach((file) => {
-            console.log(`Processing ${file}`);
+function processDirectory(directory, recurse) {
+    console.log(`Processing ${directory}${recursive && ', recursively'}`);
 
-            fs.stat(file, (err, stats) => {
-                if (stats && stats.isDirectory()) {
-                    if (recursive) {
-                        processDirectory(file);
+    fs.readdir(directory, (dirErr, files) => {
+        files.forEach((file) => {
+            if (isIgnored(file)) {
+                return;
+            }
+
+            const filePath = path.join(directory, file);
+            console.log(`Processing ${filePath}`);
+
+            fs.stat(filePath, (statErr, stats) => {
+                if (stats) {
+                    if (stats.isDirectory()) {
+                        if (recurse) {
+                            processDirectory(filePath);
+                        }
+                    } else if (stats.isFile()) {
+                        switch (path.extname(filePath).toLowerCase()) {
+                            case '.jpg':
+                            case '.jpeg':
+                                processJpeg(filePath);
+                                break;
+
+                            case '.mov':
+                                processMovie(filePath);
+                                break;
+
+                            default:
+                                console.log(`Unrecognized file type for ${filePath}`);
+                        }
                     }
                 }
             });
@@ -27,4 +51,26 @@ function processDirectory(directory) {
     });
 }
 
-processDirectory(src);
+function processJpeg(filePath) {
+    console.log(`Processing jpeg file ${filePath}`);
+}
+
+function processMovie(filePath) {
+    console.log(`Processing movie file ${filePath}`);
+}
+
+function isIgnored(file) {
+    if (path.basename(file).toLowerCase() === '.ds_store') {
+        return true;
+    }
+
+    if (path.extname(file).toLowerCase() === '.thm') {
+        return true;
+    }
+
+    return false;
+}
+
+recursive && console.log('Using recursive mode')
+
+processDirectory(src, recursive);
